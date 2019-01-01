66ba464b1552fc45c734b06ed8c434b5f56dea27
Ward Vanlerberghe <ward.vanlerberghe@aca-it.be>
2019-01-02 00:01:39 +0100

    Changed exposed port for zipkin to match jaeger config

--- distributedtracingparent/user-service/src/main/java/be/hogent/distributedtracing/UserService.java
+++ distributedtracingparent/user-service/src/main/java/be/hogent/distributedtracing/UserService.java
@@ -3,11 +3,9 @@ package be.hogent.distributedtracing;
 import be.hogent.distributedtracing.tracer.JaegerTracer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@EnableEurekaClient
 @Import(JaegerTracer.class)
 public class UserService {
 
